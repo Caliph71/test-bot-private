c4b0d8f4c142073d6decdc41e86dfdef3107e329
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-24 09:25:37 +0700

    Update upimg.js

--- plugins/upimg.js
+++ plugins/upimg.js
@@ -2,18 +2,8 @@ const fetch = require('node-fetch')
 const FormData = require('form-data')
 const { fromBuffer } = require('file-type')
 const fs = require('fs')
-async function upimg(buffer) {
-  const { ext } = await fromBuffer(buffer)
-  let form = new FormData
-  form.append('file', buffer, 'tmp.' + ext)
-  let res = await fetch('https://telegra.ph/upload', {
-    method: 'POST',
-    body: form
-  })
-  let img = await res.json()
-  if (img.error) throw img.error
-  return 'https://telegra.ph' + img[0].src
-}
+const uploadImage = require('../lib/uploadImage')
+const uploadFile = require('../lib/uploadFile')
 
  let handler  = async (m, { conn }) => {
   let q = m.quoted ? m.quoted : m
