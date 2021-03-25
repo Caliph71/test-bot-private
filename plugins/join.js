c65effc663c1c3203d9bd4b8ec93e2e6efb2f6d0
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-25 09:59:27 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -1,17 +1,22 @@
 let handler = async (m, { conn, args }) => {
   if (args.length == 0) return m.reply('Linknya mana kak')
-  pe = await conn.joinvialink(args.join(' '))
+  pe = await conn.joinvialink(args.join(' ').replace('https://chat.whatsapp.com/', ''))
+  if (!pe) return m.reply('Invalid invite link')
   conn.reply(pe.gid `Hello i am Bot, I was invited by ${conn.getName(m.sender)}`, false)
 
 }
 handler.help = ['join (link grup)']
-handler.tags = ['admin']
+handler.tags = ['main']
 handler.command = /^join$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
 handler.fail = null
 
 module.exports = handler
