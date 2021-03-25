35628fc7fa3f45219e6b376ebb3ea3b71441d900
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-25 20:03:41 +0700

    Create meme.js

---
+++ plugins/meme.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let res = await fetch(`https://recoders-area.herokuapp.com/api/random/meme?apikey=FreeApi`)
+  let json = await res.json()
+  conn.sendFile(m.chat, json.result.url, `meme.${json.result.url.split('.')[1]}`, `${json.result.title}`, m, false)
+}
+handler.help = ['meme']
+handler.tags = ['nulis']
+
+handler.command = /^(meme)$/i
+
+module.exports = handler
