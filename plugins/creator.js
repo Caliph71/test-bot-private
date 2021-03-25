ae34d424ed4bf1d32dc08ad1c17770b0005dc0c2
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-25 16:20:56 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
   F = await this.sendContact(m.chat, owner[0], m.getName(owner[0]), m)
-  
+  this.reply(m.chat, 'Ini Nomor Owner Ku, Jangan lupa Di save ya >/<', F)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
