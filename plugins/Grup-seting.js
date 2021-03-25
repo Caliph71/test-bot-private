dc8806809550852f9fd66c5f279ca3f1b9d3647d
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-25 22:10:35 +0700

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -1,22 +1,28 @@
 let { Presence, GroupSettingChange } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
-	let isClose = { // Switch Case Like :v
-		'open': false,
-		'close': true,
-	}[(args[0] || '')]
-	await conn.updatePresence(m.chat, Presence.composing)
-	if (isClose === undefined)
-		throw `
-*Format salah! Contoh :*
+
+        switch(args[0]) {
+        case 'open':
+         await conn.updatePresence(m.chat, Presence.composing)
+         await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
+         m.reply('```Sukses Membuka Grup```)
+         break
+         case 'close':
+         await conn.updatePresence(m.chat, Presence.composing)
+         await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, true)
+         m.reply('```Sukses Menutup Grup```)
+         break
+         default: 
+        m.reply(`*Format salah! Contoh :*
 
   *○ ${usedPrefix + command} close*
   *○ ${usedPrefix + command} open*
-`.trim()
-	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
+`.trim())
+      }
 }
 handler.help = ['group *open / close*']
 handler.tags = ['group']
-handler.command = /^(group)$/i
+handler.command = /^(group||grup)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
